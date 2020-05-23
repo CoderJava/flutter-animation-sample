2620671fc60b9e5fa21be6173344240fe1c9e29c
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-05-23 08:39:55 +0700

    Create animated container sample

---
+++ android/app/src/main/kotlin/com/ysn/flutteranimationsample/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ysn.flutteranimationsample
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/animated_container_sample.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double width = 200;
+  double height = 200;
+  var colorContainer = Colors.blue;
+  double radius = 16;
+  var isSmall = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Animation'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            AnimatedContainer(
+              width: width,
+              height: height,
+              curve: Curves.bounceInOut,
+              decoration: BoxDecoration(
+                color: colorContainer,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(radius),
+                ),
+              ),
+              duration: Duration(
+                seconds: 2,
+              ),
+            ),
+            RaisedButton(
+              child: Text('Animated It'),
+              onPressed: () {
+                setState(() {
+                  if (isSmall) {
+                    isSmall = false;
+                    width = 200;
+                    height = 200;
+                    colorContainer = Colors.blue;
+                    radius = 16;
+                  } else {
+                    isSmall = true;
+                    width = 100;
+                    height = 100;
+                    colorContainer = Colors.red;
+                    radius = 100;
+                  }
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
